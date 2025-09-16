82bbcd60d718f9ac07ef3319ac22f2c56e4bef08
j4ik0-dev <salazzarjefferson@gmail.com>
2025-09-16 10:00:09 -0600

    creacion de vistas y rutas para vuelos, hoteles y cruceros

--- new/src/App.vue
+++ new/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Inicio</router-link> |
+    <router-link to="/vuelos">Vuelos</router-link> |
+    <router-link to="/hoteles">Hoteles</router-link> |
+    <router-link to="/cruceros">Cruceros</router-link>
   </nav>
   <router-view/>
 </template>
--- new/src/router/index.js
+++ new/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import VuelosView from '../views/VuelosView.vue'
+import HotelesView from '../views/HotelesView.vue'
+import CrucerosView from '../views/CrucerosView.vue'
 
 const routes = [
   {
@@ -8,12 +11,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/vuelos',
+    name: 'vuelos',
+    component: VuelosView
+  },
+  {
+    path: '/hoteles',
+    name: 'hoteles',
+    component: HotelesView
+  },
+  {
+    path: '/cruceros',
+    name: 'cruceros',
+    component: CrucerosView
   }
 ]
 
