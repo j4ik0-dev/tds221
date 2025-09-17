0c668f8bbc37c82fce5c0dd5adcf948c70cc69d8
andyj2712 <aguilarelias536@gmail.com>
2025-09-16 23:26:40 -0600

    Actualizacion de la vista InicioView completa

--- new/src/views/InicioView.vue
+++ new/src/views/InicioView.vue
@@ -1,3 +1,850 @@
 <template>
-    <p>kkkklll</p>
+
+    <div id="inicio_primera_parte">
+
+    <h1>Salvadorian Airlines</h1>
+    <h2 id="inicio_slogan">Vuela por menos dinero</h2>
+    <p id="descuento_inicio">40% de descuento en billetes</p>
+
+    <div class="boton" id="boton1"><a href="">Reserva un vuelo</a></div>
+    </div>
+    <div id="inicio_segunda_parte" style="  border-top: #004AAD solid 6px;">
+    <h2>Destinos más populares</h2>
+    
+
+    <div class="destinos">
+
+    <a href="" class="destinos_opciones1">
+        <img src="https://cdn.discordapp.com/attachments/1388922256838889556/1417736915284922400/Mexico.jpg?ex=68cb91e4&is=68ca4064&hm=ce16b6106ac497a6e9ee945b7b017c5be2d480f1b603724532fb0c466f4932ac" alt="Mexico paisaje de playa">
+        <p>Mexico</p>
+    </a>
+
+    <a href="" class="destinos_opciones2">
+        <img src="https://cdn.discordapp.com/attachments/1388922256838889556/1417737091797745788/colombia.jpg?ex=68cb920e&is=68ca408e&hm=f593c0af95db2750810f2fe0e0aac7714f0a9cc3cfc041df06e829236af1c8a0" alt="Foto de pueblos Colombianos">
+        <p>Colombia</p>
+    </a>
+
+    <a href="" class="destinos_opciones3">
+        <img src="https://cdn.discordapp.com/attachments/1388922256838889556/1417737283788079185/miami.jpg?ex=68cb923c&is=68ca40bc&hm=cf6b4e5ac5e0be82db7f8ec7bfe4c987c735ee66ef9e67903d5d303ba08e3179" alt="Imagen de la ciudad de Miami">
+        <p>Miami</p>
+    </a>
+
+    </div>
+
+        <div class="boton" id="boton2"><a href="">Reserva un vuelo</a></div>
+
+</div>
+
+
+<div id="inicio_tercera_parte">
+    <div class="imagen_inscripcion">
+        <img src="https://cdn.discordapp.com/attachments/1388922256838889556/1417738774162706513/foto_inscripcion.png?ex=68cb93a0&is=68ca4220&hm=8da598020a4e3fccd748df534909660f3128788d508b5ae0c5975134d1686f33">
+    </div>
+    <div class="inscripcion">
+        <h2>No te pierdas las próximas ofertas en vuelos.</h2>
+        <p id="texto_inscripcion">Únete a la lista de correos para recibir informacion del minuto.</p>
+             <div class="boton" id="boton3"><a href="">inscríbete</a></div>
+
+    </div>
+
+</div>
+<div id="inicio_cuarta_parte">
+    <div class="alianzas_text">
+        <h2>Conexiones y alianzas con otras aerolíneas para facilitar viajes a destinos más lejanos.
+        </h2>
+    </div>
+    <div class="alianzas_img">
+        <a href="avianca.com"  class="aerolineas">
+            <img id="avianca" src="https://cdn.discordapp.com/attachments/1388922256838889556/1417736301121241158/AVIANCA.png?ex=68cb9152&is=68ca3fd2&hm=fe48fbb9eb279b8bc744efc820387c2bdd186077282aa40d21bea58496ebf75a">
+        </a>
+        <a href="https://www.aa.com/homePage.do?locale=es_PR" class="aerolineas">
+            <img id="american_airlines" src="https://cdn.discordapp.com/attachments/1388922256838889556/1417739282017292329/american-airlines.png?ex=68cb9419&is=68ca4299&hm=8d1f3ae9cb5073da571ee2046534c79388ada0d21b22b967dbfc6f249bbabb30">
+        </a>
+        <a href="https://www.volaris.com/?gad_source=1&gclid=Cj0KCQiAn-2tBhDVARIsAGmStVnDPig2gWoEUTVvN08AujpCk7dCGxunqi30gP90GPmSZz963moM6KMaAigtEALw_wcB" class="aerolineas">
+            <img id="volaris" src="https://cdn.discordapp.com/attachments/1388922256838889556/1417739489182486558/volaris.png?ex=68cb944a&is=68ca42ca&hm=a092b96c5cf180e1b4565b36427dd48580119839610170261f7bd43f00b68743">
+        </a>
+
+    </div>
+</div>
+
+
+
+
+<div id="inicio_sexta_parte">
+
+    <div class="beneficios_texto">
+        <h2>Lo que incluyen vuelos a todos los destinos*.</h2>
+        <p>Tu seguridad y confianza son nuestra prioridad, implementamos medidas para garantizar la seguridad de todos nuestros pasajeros.</p>
+        <p id="aplica_beneficios">*No APLICA para primera clase</p>
+    </div>
+    <div class="lista_beneficios">
+        <ul>
+            <li>Wifi OnAir gratuito</li>
+            <li>Diferentes opciones de comida abordo</li>
+            <li>Comodidad en asientos y espacio</li>
+            <li>Entretenimiento ilimitado</li>
+          </ul>
+          
+    </div>
+</div>
 </template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&family=Unbounded:wght@200..900&display=swap');
+.unbounded {
+    font-family: "Unbounded", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: weight;
+    font-style: normal;
+  }
+  .arsenal-regular {
+      font-family: "Arsenal", sans-serif;
+      font-weight: 400;
+      font-style: normal;
+    }
+    
+    .arsenal-bold {
+      font-family: "Arsenal", sans-serif;
+      font-weight: 700;
+      font-style: normal;
+    }
+    
+    .arsenal-regular-italic {
+      font-family: "Arsenal", sans-serif;
+      font-weight: 400;
+      font-style: italic;
+    }
+    
+    .arsenal-bold-italic {
+      font-family: "Arsenal", sans-serif;
+      font-weight: 700;
+      font-style: italic;
+    }
+    
+:root {
+    --color-primario: #004AAD;
+    --color-tercero: #5092e9;
+    --color-secundario: #ecf2f7;
+    --font-titulo: "Arsenal", sans-serif;
+    --font-subtitulo: "Unbounded", sans-serif;
+    --font-texto: Arial, Helvetica, sans-serif;
+
+  }
+  
+*{
+    box-sizing: border-box;
+    margin: 0;
+}
+
+.boton{
+  border-radius: 30px;
+  background-color: #004AAD;
+  padding: 10px;
+  
+
+}
+.boton a{
+  width: fit-content;
+  text-decoration: none;
+  color: var(--color-secundario);
+  text-transform: uppercase;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.boton a:hover{
+  font-weight: bold;
+}
+
+div#inicio_primera_parte{
+  background-image: url(https://cdn.discordapp.com/attachments/1388922256838889556/1417734967127179386/Azul.png?ex=68cb9014&is=68ca3e94&hm=67d970c54a00957a7451015269966339b3cb7a0c6396421efecf3303943036f5);
+  background-size: cover;
+  position: abso; height: 100vh; width: 100%;
+  margin: 0;
+  display: block;
+  background-repeat: no-repeat;
+
+}
+
+nav{
+  margin:0;
+  width: 100%;
+  height: 7vh;
+  background-color: var(--color-primario);
+  justify-content: center;
+  position: absolute;
+
+}
+nav ul{
+  position: relative;
+  left: 50%;
+  top: 2vh;
+  transform: translate(-50%, -50%);
+  width: 70vw;
+  display: inline-flex;
+  list-style: none;
+  justify-content: space-between;
+  z-index: 2;
+
+}
+
+
+div.dropdown-menu{
+  color: var(--color-secundario);
+}
+a.dropdown-item{
+  color: var(--color-primario);
+  font-weight: bold;
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+;}
+
+nav ul li{
+  display: flex;
+  font-family: Arial, Helvetica, sans-serif;
+
+}
+nav ul li a{
+  font-size: small;
+  font-weight: bolder;
+  padding: 10px;
+  color: var(--color-secundario);
+  text-decoration: none;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+nav ul li a:hover{
+  color: var(--color-tercero);
+
+}
+h1{
+  font-size: 40px;
+  width: fit-content;
+  position: relative;
+  left: 50%;
+  top: 150px;
+  transform: translate(-50%);
+  font-family: "Arsenal", sans-serif;
+  font-style: italic; 
+  color: var(--color-secundario);
+
+}
+h2#inicio_slogan{
+  height: fit-content;
+  width: fit-content;
+  text-align: center;
+  position: absolute;
+  justify-content: center;  
+  top: 270px;
+  left: 50%;
+  transform: translate(-50%);
+  font-family: "Unbounded", sans-serif;
+  font-size: 40px;
+  color: var(--color-secundario);
+}
+p#descuento_inicio{
+  height: fit-content;
+  width: fit-content;
+  position: absolute;
+  justify-content: center;  
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 30px;
+  color: var(--color-secundario);
+  font-weight:100;
+}
+div#boton1{
+  z-index: 2;
+  position: absolute;
+  justify-content: center;
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%);
+ 
+}
+
+div#iinicio_segunda_parte{
+  
+  position: relative;
+  top: 100%;
+  width: 100%;
+  margin: 0;
+  display: flex;
+  background-repeat: no-repeat;
+  background-color: var(--color-secundario);
+  height: 100vh;
+
+
+}
+
+#inicio_segunda_parte h2{
+  font-size: 40px;
+  width: fit-content;
+  position: relative;
+  left: 50%;
+  top: 150px;
+  transform: translate(-50%);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: black;
+  font-weight: 400;
+  height: 30vh;
+}
+
+div.destinos{
+  width: 100%;
+  position: relative;
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+
+}
+
+.destinos_opciones1 p,.destinos_opciones2 p, .destinos_opciones3 p{
+  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+div.destinos a {
+  margin: 5vw;
+  width: 20vw;
+  height: 20vw;
+  overflow: hidden; 
+  position: relative;
+  margin-bottom: 100px;
+}
+div.destinos p{
+
+  z-index: 2;
+  width: fit-content;
+  position: absolute;
+  color: var(--color-secundario);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 400;
+  font-size: 35px;
+}
+
+
+div.destinos a img {
+  width: 100%;
+  height: 100%; 
+  object-fit: cover; 
+}
+
+#boton2{
+  position: relative;
+  width: fit-content;
+  background-color: var(--color-tercero);
+  left: 50%;
+  transform: translate(-50%);
+  margin-bottom: 100px;
+}
+img{
+  width: 500px;
+}
+
+div#inicio_tercera_parte{
+  background-color: var(--color-tercero);
+  position: relative;
+}
+div#imagen_inscripcion{
+  position: relative;
+}
+div.imagen_inscripcion img{
+  margin: 0px;
+  padding: 0%;
+  position: relative;
+  height: 100vh;
+  left: 10vw;
+}
+div.inscripcion{
+  width: 400px;
+  position: absolute;
+  left: 60vw;
+  top: 30vh;
+}
+div.inscripcion h2{
+  color: var(--color-secundario);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 500;
+  font-size: 45px;
+}
+#texto_inscripcion{
+  font-size: 24px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 100;
+  color: var(--color-secundario);
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+#boton3{
+  position: relative;
+  width: fit-content;
+  background-color: var(--color-secundario);
+  margin-bottom: 200px;
+}
+#boton3 a{
+  color: var(--color-primario);
+}
+
+div#inicio_cuarta_parte{
+  background-color: rgba(201, 201, 201, 0.473);
+position: static;
+height: 7%;
+justify-content: center;
+padding-bottom: 200px;
+}
+
+#inicio_cuarta_parte h2{
+  text-align: center;  
+  position: relative;
+  left: 50vw;
+  transform: translate(-50%);
+  top: 7vh;
+  width: 70vw;
+  font-size: 45px;
+  font-weight: 400;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+div.alianzas_img{
+  padding-top: 20vh;
+  width: 80%;
+  position: relative;
+  left: 50vw;
+  transform: translate(-50%);
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;}
+  
+
+div.alianzas_img a.aerolineas{
+  width: 300px;
+  height: 200px;
+  position: relative;
+  object-fit: contain;
+  background-color: white;
+}
+.aerolineas #avianca{
+  width: 70%;
+}
+div.alianzas_img a.aerolineas img{
+  position: inherit;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  object-fit: contain; 
+}
+
+#inicio_quinta_parte{
+  position: relative;
+  background-image: url(https://cdn.discordapp.com/attachments/1388922256838889556/1417735362176090123/altitud.png?ex=68cb9072&is=68ca3ef2&hm=14e31088e800d65ca6ce5113f251ccf76a08add57e063c07df15d795c406a06d);
+  filter: hue-rotate(50%);
+  background-position:100%;
+  height: 100vh;
+}
+#altitud_texto1{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 500;
+  font-size: 45px;
+  width: 40vw;
+  margin-left: 25px;
+  color: white;
+  text-shadow: -10px 10px 80px rgb(8, 78, 95);
+}
+div#altitud_texto2{
+  background-color: var(--color-secundario);
+  position: relative;
+  width: 35vw;
+  left: 63vw;
+  top: -5vw;
+  padding: 40px;
+  
+}
+
+div#altitud_texto2 p{
+font-size: 25px;
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+#boton4{
+  width: fit-content;
+  background-color: var(--color-primario);
+  left: 50%;
+  margin-top: 20px;
+  margin-bottom: 0%;
+}
+#inicio_sexta_parte{
+  background-color: var(--color-secundario);
+  height: 100vh;
+  position: relative;
+}
+div.beneficios_texto{
+  width: 35vw;
+  position: relative;
+  left: 30%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+div.beneficios_texto h2{
+  font-family: "Arsenal", sans-serif;
+  font-size: 50px;
+  margin-bottom: 30px;
+
+}
+div.beneficios_texto p{
+  width: 35vw;
+  font-family: "Arsenal", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+}
+#aplica_beneficios{
+  font-weight: 900;
+}
+
+div.lista_beneficios{
+  position: absolute;
+  width: 40vw;
+  padding: 20px;
+  padding-top: 70px;
+  padding-bottom: 70px;
+  background-color: var(--color-primario);
+  left: 75%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+}
+div.lista_beneficios ul{
+  color: var(--color-secundario);
+  font-size: 2vw;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+div.lista_beneficios li{
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+footer{
+  height: 47vh;
+  background-color: var(--color-primario);
+}
+div.redes{
+  width: 200px;
+  height: fit-content;
+  position: relative;
+  left: 10%;
+  top: 20%;
+
+}
+div.redes p{
+  color: white;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 15px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+div.redes img{
+  width: 45px;
+  margin-bottom: 20px;
+}
+div.redes a{
+  text-decoration: none;
+  font-size: 15px;
+  color: white;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+#correo_oficial{
+  font-weight: bold;
+}
+
+div.sucursales{
+  width: fit-content;
+  position: relative;
+  top: -15%;
+  left: 35%;
+  display: inline-flex;
+  line-height: 35px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+}
+#sedes{
+  color: white;
+  position: absolute;
+  top: -1%;
+  font-weight: bold;
+}
+
+div#Sucursal_sv{
+  margin-right: 10px;
+}
+
+#copyright{
+  color: white;
+  position: absolute;
+  padding-left: 20px;
+  padding-top: 10px;
+  width: 100%;
+  border-top: solid 1px white;
+}
+
+
+
+@media (max-width: 1071px) {
+
+  nav div.collapse ul{
+      width: 80vw;
+  }
+ 
+  
+  footer{
+    height: 100vh;
+    text-align: center;
+  }
+  div.redes{
+    top: 10%;
+    width: 40vw;
+    left: 50%;
+    transform: translate(-50%);
+    text-align: center;}
+
+   a#correo_oficial{
+      padding: 3vw;
+    }
+  
+  
+  div.redes p{
+    font-size: 20px;
+    text-align: center;
+  }
+  
+  div.redes img{
+    width: 10%;
+    margin-bottom: 20px;
+  }
+  div.redes a{
+    align-items: center;
+  
+  }
+  
+  p#sedes{
+  width: fit-content;    
+  left: 50%;
+  transform: translate(-50%);
+  font-size: 3vw;
+  }
+  
+  div.sucursales{
+    
+    top: 15%;
+    left: 50%;
+    transform: translate(-50%);
+    display: block;
+    
+  }
+  
+  
+  #copyright{
+    position: relative;
+    bottom: -25%;
+
+  }
+
+
+  h2#inicio_slogan{
+    text-align: center;
+  
+
+  }
+  p#descuento_inicio{
+    text-align: center;
+    margin-top: 5vh;
+    }
+
+   
+    div.imagen_inscripcion img{
+      height: 110vh;
+      left: 50vw;
+      transform: translate(-50%);
+    }
+    div.inscripcion{
+      background-color: var(--color-primario);
+      left: 50vw;
+      transform: translate(-50%, -50%);
+      padding: 2.5vw;
+      top: 50%;
+    }
+    #boton3{
+      left: 50%;
+      transform: translate(-50%);    
+      margin-top: 5vh; 
+      margin-bottom: 0%;
+    }
+
+
+
+    #inicio_cuarta_parte h2{
+      font-size: 4vw;
+    }
+
+    div.alianzas_img{
+      width: fit-content;
+      display: grid;
+      height: 90vw;
+      left: 50%;
+      transform: translate(-50%);
+    }
+    div.alianzas_img a.aerolineas{
+      width: 50vw;
+      height: 20vw;}
+
+
+
+    div.alianzas_img img{
+      display: block;
+      border-top: 5px;
+      position: relative;
+    }
+
+    div.alianzas_img .aerolineas{
+    position: relative;}
+
+    #altitud_texto1{
+      position: relative;
+      top: 5vw;
+      font-size: 3vw;
+      width: fit-content;
+      text-align: center;
+      color: rgb(94, 89, 82);
+    }
+    div#altitud_texto2{
+      text-align: center;
+      width: 80vw;
+      left: 50vw;
+      transform: translate(-50%);
+      top: 15vw;
+      padding: 30px;
+      font-size: smaller;
+      
+    }
+    #boton4{
+      position: relative;
+      left: 50%;
+      transform: translate(-50%);
+     
+    }
+    
+
+    #inicio_sexta_parte h2{
+      text-align: center;
+      font-size: 5vw;
+    }
+
+    div.lista_beneficios ul{
+      font-size: 2.5vw;
+      
+    }
+
+  }
+
+  
+  @media (max-width: 990px) {
+
+    nav{
+      z-index: 1;;
+    }
+    nav{
+      z-index: 1;
+      height: 10vh;
+    }
+  
+    nav ul{
+      margin-left: 10px;
+      top: 17vh;
+      background-color: var(--color-primario);
+      width: 90vw;
+      transition:linear 0.001s;
+      z-index: 0;
+    }
+
+    nav ul li{
+      padding-left: 3%;
+    }
+   
+    .navbar-toggler{
+      z-index: 1;
+  
+    }
+    .navbar-toggler-icon{
+      z-index: 1;
+  
+    }
+    div.dropdown-menu{
+      border: none;
+      margin-left: 0%;
+      width: 100%;
+      background-color: var(--color-primario);
+    }
+
+    #navbarSupportedContent{
+      width: 100px;
+    }
+    a.nav-link{
+      width: 40%;
+      margin-bottom: 2%;
+    }
+
+    a.dropdown-item{
+      border-left: solid 1px var(--color-secundario);
+      color: var(--color-secundario);
+      font-weight: bolder;
+    }
+    a.dropdown-item:hover{
+      color: rgb(162, 166, 168);
+      background-color: inherit;
+    }
+
+  }
+
+  @media (max-width: 700px) {
+
+    nav ul{
+      top: 19vh;
+      padding-top: 5%;
+      background-color: var(--color-primario);
+      width: 90vw;
+      transition: ease-in 0.5s;
+    }
+  
+    nav{
+      z-index: 1;
+    }
+
+  
+  h2#inicio_slogan{
+      width: 90vw;}
+
+  p#descuento_inicio{
+      margin-top: 5%;
+      }
+
+  div#inicio_segunda_parte div.destinos{
+    width: 50vw;
+    display: grid;
+    left: 50vw;
+    transform: translate(-50%);
+  }
+
+  div#inicio_segunda_parte div.destinos a{
+    margin: 0%;
+    margin-bottom: 3vw;
+    width: 50vw;
+
+
+  }
+  }
+</style>
